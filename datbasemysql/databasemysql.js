baa356fa465ce4a4a3aa6aa4d239b49b3539252c
martin <tincho8919@gmail.com>
2024-01-08 20:30:20 -0300

    basededatoslocal

--- datbasemysql/databasemysql.js
+++ datbasemysql/databasemysql.js
@@ -5,11 +5,11 @@ const mysql = require ("mysql2")
 dotenv.config();
 
 const dbmysql = mysql.createConnection({
-    host: 'bbq56ezy88vymselwqml-mysql.services.clever-cloud.com',
-    user: 'u0w1f1reo1dtcers',
-    password: 'jGkAW10PPaa3OLzcGxKy',
+    host: 'localhost',
+    user: 'root',
+    password: '34697923Fm@',
     port: '3306',
-    database: 'bbq56ezy88vymselwqml'
+    database: 'SUPERHELADOS'
    
 
 });
@@ -21,7 +21,7 @@ dbmysql.connect((err) =>{
         console.error('Error en la conexión a la base de datos:', err);
         return;
       }
-    console.log(`Conexion a la database ${'bbq56ezy88vymselwqml'} conexion exitosa`);
+    console.log(`Conexion a la database ${'SUPERHELADOS'} conexion exitosa`);
         
 });
 
